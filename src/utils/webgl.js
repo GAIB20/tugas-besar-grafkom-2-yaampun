64ad9d92d4da2eccf5479e0908c67de1701f64c0
febryanarota <arota0202@gmail.com>
2024-05-13 03:56:04 +0700

    setup webgl

---
+++ src/utils/webgl.js
@@ -0,0 +1,91 @@
+function createShaderProgram(gl, vertexShaderTxt, fragmentShaderTxt) {
+    vShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vShader, vertexShaderTxt);
+    gl.compileShader(vShader);
+
+    const vertexShader = vShader;
+
+    fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fShader, fragmentShaderTxt);
+    gl.compileShader(fShader);
+
+    const fragmentShader = fShader;
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+
+    return program;
+}
+
+function setAttr(gl, program, a_position, a_normal, a_color, a_texture, a_tangent, a_bitangent) {
+
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, a_position, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aPosition"));
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aPosition"), 3, gl.FLOAT, false, 0, 0);
+  
+    const normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, a_normal, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aNormal"));
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aNormal"), 3, gl.FLOAT, false, 0, 0);
+  
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, a_color, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aColor"), 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aColor"));
+  
+    const textureBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, a_texture, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aTexture"), 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aTexture"));
+  
+    const vbo_tang = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_tang);
+    gl.bufferData(gl.ARRAY_BUFFER, a_tangent, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aTangent"), 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aTangent"));
+  
+    const vbo_bitang = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_bitang);
+    gl.bufferData(gl.ARRAY_BUFFER, a_bitangent, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.getAttribLocation(program, "aBitangent"), 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.getAttribLocation(program, "aBitangent"));
+}
+
+function setUniforms(gl, program, uniforms) {
+    worldViewProjection = gl.getUniformLocation(program, "uWorldViewProjection");
+    worldInverseTranspose = gl.getUniformLocation(program,"uWorldInverseTranspose");
+    color = gl.getUniformLocation(program, "uColor");
+    reverseLightDirection = gl.getUniformLocation(program,"uReverseLightDirection");
+    texture =  gl.getUniformLocation(program, "uTexture");
+    worldCameraPosition = gl.getUniformLocation(program, "uWorldCameraPosition");
+    modelMatrix = gl.getUniformLocation(program, "uModelMatrix");
+  
+    gl.uniformMatrix4fv(
+      worldViewProjection,
+      false,
+      uniforms.uWorldViewProjection
+    );
+    gl.uniformMatrix4fv(
+      worldInverseTranspose,
+      false,
+      uniforms.uWorldInverseTranspose
+    );
+    gl.uniform4fv(color, uniforms.uColor);
+    gl.uniform3fv(
+      reverseLightDirection,
+      uniforms.uReverseLightDirection
+    );
+    gl.uniform1i(texture, 0);
+    gl.uniformMatrix4fv(modelMatrix, false, uniforms.uModelMatrix);
+}
